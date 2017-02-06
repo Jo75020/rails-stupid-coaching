89f8e69b5aab1b4c4a7deb38d419323f8021ba24
Jordan Demmouche <jordand75020@hotmail.fr>
2017-02-06 12:37:51 +0100

    end

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,16 @@
+class CoachingController < ApplicationController
+  def answer
+    if params[:query]  ==  "I am going to work right now!"
+      @answer = "You rock !"
+    elsif params[:query].include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+
+
+end
